58cc2418a0669b081486124b42e740f1e178fa86
sandsq <116196797+sandsq@users.noreply.github.com>
2025-03-05 12:24:38 -0500

    testing: add test for different battle states

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,8 @@ using Logging
 debuglogger = ConsoleLogger(stderr, Logging.Debug)
 Base.global_logger(debuglogger)
 
-source = Entity("Kingambit", 50)
-target = Entity("Kyurem", 95)
+kingambit = Entity("Kingambit", 50)
+kyurem = Entity("Kyurem", 95)
 
 @testset "Move order" begin
     @test status_move < preemptive_attack
@@ -14,24 +14,24 @@ target = Entity("Kyurem", 95)
 end
 
 @testset "Actions" begin
-    a = Action(source, target, preemptive_attack)
-    @test a == Action(source, target, preemptive_attack)
+    a = Action(kingambit, kyurem, preemptive_attack)
+    @test a == Action(kingambit, kyurem, preemptive_attack)
     @test describe(a) == "[Kingambit, 50.0 speed] used PreemptiveAttack() on [Kyurem, 95.0 speed]"
     
-    a2 = Action(target, source, status_move)
+    a2 = Action(kyurem, kingambit, status_move)
     @test a > a2
 
-    a3 = Action(source, target, regular_attack)
+    a3 = Action(kingambit, kyurem, regular_attack)
     @test a2 > a3
 end
 
 
 @testset "States" begin
-    a1 = Action(target, source, regular_attack)
-    a2 = Action(source, target, preemptive_attack)
+    a1 = Action(kyurem, kingambit, regular_attack)
+    a2 = Action(kingambit, kyurem, preemptive_attack)
     t = TurnState([a1, a2])
 
-    a3 = Action(source, target, regular_attack)
+    a3 = Action(kingambit, kyurem, regular_attack)
     t2 = TurnState([a3, a1])
 
     @testset "TurnState sorting" begin
@@ -43,7 +43,7 @@ end
     end
 
     
-    b = BattleState(target, source, [t, t2])
+    b = BattleState(kyurem, kingambit, [t, t2])
 
     @testset "BattleState" begin
         @test b.turns[1] == t
@@ -52,53 +52,81 @@ end
 
     @testset "victory conditions" begin
         @testset "both regular attacks" begin
-            a1 = Action(source, target, regular_attack)
-            a2 = Action(target, source, regular_attack)
+            a1 = Action(kingambit, kyurem, regular_attack)
+            a2 = Action(kyurem, kingambit, regular_attack)
             t = TurnState([a1, a2])
-            b = BattleState(source, target, [t])
-            @test get_victor(b) == (target, 1)
+            b = BattleState(kingambit, kyurem, [t])
+            @test get_victor(b) == (kyurem, 1)
         end
 
         @testset "both status moves" begin
-            a1 = Action(source, target, status_move)
-            a2 = Action(target, source, status_move)
+            a1 = Action(kingambit, kyurem, status_move)
+            a2 = Action(kyurem, kingambit, status_move)
             t = TurnState([a1, a2])
-            b = BattleState(source, target, [t])
+            b = BattleState(kingambit, kyurem, [t])
             @test get_victor(b) == (nothing, -1)
         end
 
         @testset "regular vs status" begin
-            a1 = Action(source, target, regular_attack)
-            a2 = Action(target, source, status_move)
+            a1 = Action(kingambit, kyurem, regular_attack)
+            a2 = Action(kyurem, kingambit, status_move)
             t = TurnState([a1, a2])
-            b = BattleState(source, target, [t])
-            @test get_victor(b) == (source, 1)
+            b = BattleState(kingambit, kyurem, [t])
+            @test get_victor(b) == (kingambit, 1)
         end
 
         @testset "preemptive vs regular" begin
-            a1 = Action(source, target, preemptive_attack)
-            a2 = Action(target, source, regular_attack)
+            a1 = Action(kingambit, kyurem, preemptive_attack)
+            a2 = Action(kyurem, kingambit, regular_attack)
             t = TurnState([a2, a1])
             @test is_preemptive_successful(t)
-            b = BattleState(source, target, [t])
-            @test get_victor(b) == (source, 1)
+            b = BattleState(kingambit, kyurem, [t])
+            @test get_victor(b) == (kingambit, 1)
         end
 
         @testset "preemptive vs status" begin
-            a1 = Action(source, target, preemptive_attack)
-            a2 = Action(target, source, status_move)
+            a1 = Action(kingambit, kyurem, preemptive_attack)
+            a2 = Action(kyurem, kingambit, status_move)
             t = TurnState([a2, a1])
             @test !is_preemptive_successful(t)
-            b = BattleState(source, target, [t])
+            b = BattleState(kingambit, kyurem, [t])
             @test get_victor(b) == (nothing, -1)
         end
 
-        @testset "preemptive vs status pp stall" begin
-            a1 = Action(source, target, preemptive_attack)
-            a2 = Action(target, source, status_move)
+        @testset "preemptive vs successful status pp stall" begin
+            a1 = Action(kingambit, kyurem, preemptive_attack)
+            a2 = Action(kyurem, kingambit, status_move)
             t = TurnState([a2, a1])
-            b = BattleState(source, target, [t for i in 1:8])
-            @test get_victor(b) == (target, 8)
+            b = BattleState(kingambit, kyurem, [t for i in 1:8])
+            @test get_victor(b) == (kyurem, 8)
+        end
+
+        @testset "failed status pp stall" begin
+            a1 = Action(kingambit, kyurem, preemptive_attack)
+            a1_5 = Action(kingambit, kyurem, regular_attack)
+            a2 = Action(kyurem, kingambit, status_move)
+            t = TurnState([a2, a1])
+            b = BattleState(kingambit, kyurem, append!([t for i in 1:4], [TurnState(a1_5, a2)], [t for i in 1:4]))
+            @test get_victor(b) == (kingambit, 5)
+        end
+
+        @testset "partial pp stall into attack on preemptive" begin
+            a1 = Action(kingambit, kyurem, preemptive_attack)
+            a2 = Action(kyurem, kingambit, status_move)
+            a2_5 = Action(kyurem, kingambit, regular_attack)
+            t = TurnState([a2, a1])
+            b = BattleState(kingambit, kyurem, append!([t for i in 1:5], [TurnState(a2_5, a1)], [t for i in 1:3]))
+            @test get_victor(b) == (kingambit, 6)
+        end
+
+        @testset "partial pp stall into attack on attack" begin
+            a1 = Action(kingambit, kyurem, preemptive_attack)
+            a1_5 = Action(kingambit, kyurem, regular_attack)
+            a2 = Action(kyurem, kingambit, status_move)
+            a2_5 = Action(kyurem, kingambit, regular_attack)
+            t = TurnState([a2, a1])
+            b = BattleState(kingambit, kyurem, append!([t for i in 1:6], [TurnState(a2_5, a1_5)], [t for i in 1:2]))
+            @test get_victor(b) == (kyurem, 7)
         end
     end
     
